abc5a316bcc867c6b7971b4e204ffbf97c26130f
tomgp <tom.g.pearson@gmail.com>
2016-04-26 14:56:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var request = require('request');
+var nunjucks = require('nunjucks');
+
+
+
+var config = JSON.parse(fs.readFileSync('config.json','utf-8'));
+
+console.log(config);
+
+
+if(config.bertha){
+	request(config.bertha, function (err, response, body) {
+		if (!err && response.statusCode == 200) {
+			var sheets = JSON.parse(body);
+			var partyLookup = makeLookup(sheets.parties, 'paabbreviation');
+
+			nunjucks.configure('views', {
+			    autoescape: true
+			})
+			.addFilter('partyname', function(code){
+				console.log(code);
+				if(partyLookup[code]){ 
+					return partyLookup[code].name; 
+				}
+				return code			
+			});
+
+			//render html
+			var html = nunjucks.render('template.html', sheets)
+			//save html
+			fs.writeFileSync(config.htmlDir + '/index.html', html);
+		}else{
+			console.error(new Date() + " " + err);
+			exit(1);
+		}
+	})
+}else{
+	console.error(new Date() + " no bertha sheet specified");
+	exit(1);
+}
+
+
+function makeLookup(array, key){
+	var lookup = {};
+	array.forEach(function(d){
+		lookup[ d[key] ] = d;
+	});
+	return lookup;
+}
